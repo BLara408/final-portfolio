e3c4dbe69a7973274e17f8d19b23ca2cb671cd90
Brandon Lara <larabrandon408@gmail.com>
2021-10-19 14:16:09 -0700

    init

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './components/Cards.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
